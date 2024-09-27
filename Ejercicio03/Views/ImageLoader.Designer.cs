e013c4546bbd384a9602ff73307ca11e8feaa32f
raulm <raulm@VMRAUL>
2024-09-27 10:15:04 +0200

    xdvvnb

--- Ejercicio03/Views/ImageLoader.Designer.cs
+++ Ejercicio03/Views/ImageLoader.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // ImageLoader
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(467, 349);
+            this.Name = "ImageLoader";
+            this.Text = "Image Loader";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
