5b1413ff66b8ff71b95df5dcefcbca7d33a8be5a
raulm <raulm@VMRAUL>
2024-09-27 12:37:18 +0200

    FALTA PONER URL A IMAGELOADER

--- Ejercicio03/Views/ImageLoader.Designer.cs
+++ Ejercicio03/Views/ImageLoader.Designer.cs
@@ -28,20 +28,74 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ImageLoader));
+            this.URLInput = new System.Windows.Forms.TextBox();
+            this.loadImageButton = new System.Windows.Forms.Button();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.OpenFileButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
             this.SuspendLayout();
+            // 
+            // URLInput
+            // 
+            this.URLInput.Location = new System.Drawing.Point(16, 58);
+            this.URLInput.Name = "URLInput";
+            this.URLInput.Size = new System.Drawing.Size(320, 20);
+            this.URLInput.TabIndex = 0;
+            // 
+            // loadImageButton
+            // 
+            this.loadImageButton.Location = new System.Drawing.Point(355, 57);
+            this.loadImageButton.Name = "loadImageButton";
+            this.loadImageButton.Size = new System.Drawing.Size(47, 23);
+            this.loadImageButton.TabIndex = 1;
+            this.loadImageButton.Text = "Load";
+            this.loadImageButton.UseVisualStyleBackColor = true;
+            this.loadImageButton.Click += new System.EventHandler(this.loadImageButton_Click);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox.Image")));
+            this.pictureBox.Location = new System.Drawing.Point(24, 103);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(418, 219);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox.TabIndex = 2;
+            this.pictureBox.TabStop = false;
+            // 
+            // OpenFileButton
+            // 
+            this.OpenFileButton.Location = new System.Drawing.Point(408, 57);
+            this.OpenFileButton.Name = "OpenFileButton";
+            this.OpenFileButton.Size = new System.Drawing.Size(47, 23);
+            this.OpenFileButton.TabIndex = 3;
+            this.OpenFileButton.Text = "Open";
+            this.OpenFileButton.UseVisualStyleBackColor = true;
+            this.OpenFileButton.Click += new System.EventHandler(this.openButton_click);
             // 
             // ImageLoader
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(467, 349);
+            this.Controls.Add(this.OpenFileButton);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.loadImageButton);
+            this.Controls.Add(this.URLInput);
             this.Name = "ImageLoader";
             this.Text = "Image Loader";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox URLInput;
+        private System.Windows.Forms.Button loadImageButton;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Button OpenFileButton;
     }
 }
 
--- Ejercicio03/Views/ImageLoader.cs
+++ Ejercicio03/Views/ImageLoader.cs
@@ -2,19 +2,93 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics.Eventing.Reader;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Ejercicio03
 {
     public partial class ImageLoader : Form
+
+
     {
         public ImageLoader()
         {
-            InitializeComponent();
+        InitializeComponent();
+        }
+
+        private void loadImageButton_Click(object sender, EventArgs e)
+        {
+
+            Regex linuxFilePath = new Regex("^\\/.*(?! )");
+            string wfp = @"^([A-Z]:\\).*";
+            Regex windowsFilePath = new Regex(wfp);
+            Regex imageFilePattern = new Regex("((\\.jpeg)|(\\.jpg)|(\\.png)|(\\.bmp)|(\\.avif))");
+
+
+            if (linuxFilePath.IsMatch(URLInput.Text))
+            {
+                MessageBox.Show("Linux File Path is not supported :D");
+            }
+            else if (windowsFilePath.IsMatch(URLInput.Text))
+            {
+                if (File.Exists(URLInput.Text))
+                {
+                    if (imageFilePattern.IsMatch(URLInput.Text))
+                    {
+                        pictureBox.ImageLocation = URLInput.Text;
+                    }
+                }
+            }
+        }
+
+        private void openButton_click(object sender, EventArgs e)
+        {
+            Regex linuxFilePath = new Regex("^\\/.*(?! )");
+            string wfp = @"^([A-Z]:\\).*";
+            Regex windowsFilePath = new Regex(wfp);
+            Regex imageFilePattern = new Regex("((\\.jpeg)|(\\.jpg)|(\\.png)|(\\.bmp)|(\\.avif))");
+
+
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.InitialDirectory = "C:";
+            ofd.Filter = "Image Files| *.BMP; *.JPG; *.GIF; *.JPEG;)";
+
+            if (linuxFilePath.IsMatch(URLInput.Text))
+            {
+                MessageBox.Show("Linux File Path is not supported :D");
+            }
+            else if (windowsFilePath.IsMatch(URLInput.Text))
+            {
+                try
+                {
+                    ofd.InitialDirectory = URLInput.Text;
+                    if (ofd.ShowDialog() == DialogResult.OK)
+                    {
+                        URLInput.Text = ofd.FileName;
+                    }
+                    pictureBox.ImageLocation = ofd.FileName;
+
+                } catch (Exception) {
+                    MessageBox.Show("Not Found");
+                }
+            } else
+            {
+                ofd.ShowDialog();
+                if (File.Exists(ofd.FileName))
+                {
+                    URLInput.Text = ofd.FileName;
+                    if (imageFilePattern.IsMatch(ofd.FileName))
+                    {
+                        pictureBox.ImageLocation = ofd.FileName;
+                    }
+                }                
+            }
         }
     }
 }
